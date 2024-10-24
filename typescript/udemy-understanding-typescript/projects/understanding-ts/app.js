ed8cf4e3cba0348e566759297713a32ca3b0ee48
ALVIN-DESKTOP/Alvin Toh <alvin4292@gmail.com>
2024-10-24 23:52:29 +1100

    feat(section2): finished until 23. Union Types, and learn the core types of typescript

--- typescript/udemy-understanding-typescript/projects/understanding-ts/app.js
+++ typescript/udemy-understanding-typescript/projects/understanding-ts/app.js
@@ -1,9 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-var person = {
-    name: "Maximilian",
-    age: 30,
-};
-console.log(person.name);
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedStringAges = combine("Max", "Anna");
+console.log(combinedStringAges);
