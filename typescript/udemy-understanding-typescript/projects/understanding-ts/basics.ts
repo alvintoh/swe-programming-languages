68d6b75fe38799fee4e63f981aeab021cc1bf3a4
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2024-10-29 23:12:26 +1100

    feat(section2): finished until 33. Useful Resources & Links with different TypeScript types in ts classes in understanding-ts project

--- typescript/udemy-understanding-typescript/projects/understanding-ts/basics.ts
+++ typescript/udemy-understanding-typescript/projects/understanding-ts/basics.ts
@@ -10,7 +10,8 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-let number1 = 5; // 5.0
+let number1: number;
+number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = "Result is: ";
