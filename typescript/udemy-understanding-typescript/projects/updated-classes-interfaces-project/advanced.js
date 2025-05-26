b0807adce1d6f3fd91a750ac7f86a0284a852efd
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2025-05-26 23:02:53 +1000

    feat(section6): Finishing updated content for 2025 until 87. How Interfaces Get (Not) Compiled To JavaScript

--- typescript/udemy-understanding-typescript/projects/updated-classes-interfaces-project/advanced.js
+++ typescript/udemy-understanding-typescript/projects/updated-classes-interfaces-project/advanced.js
@@ -6,7 +6,7 @@ class AdvancedUser {
     }
     set firstName(name) {
         if (name.trim() === "") {
-            throw new Error("Invalid name");
+            throw new Error("Invalid first name");
         }
         this._firstName = name;
     }
@@ -27,5 +27,35 @@ AdvancedUser.eid = "USER";
 console.log(AdvancedUser.eid); // Accessing static property
 const alv = new AdvancedUser();
 alv.firstName = "Alvin";
-alv.lastName = "Toh";
+alv.lastName = "Doe";
+// alv._firstName = "Alvin 2";
 console.log(alv.fullName);
+class Employee extends AdvancedUser {
+    constructor(jobTitle) {
+        super();
+        this.jobTitle = jobTitle;
+        // super.firstName = Alvin;
+    }
+    work() {
+        // ...
+        console.log(this._firstName);
+        // super._firstName
+    }
+}
+class UIElement {
+    constructor(identifier) {
+        this.identifier = identifier;
+    }
+    clone(targetLocation) {
+        // logic to duplicate the UI element
+    }
+}
+// let uiElement = new UIElement
+// Abstract classes should only be used as base classes to be extended from
+class SideDrawerElement extends UIElement {
+    constructor(identifier, position) {
+        super(identifier);
+        this.identifier = identifier;
+        this.position = position;
+    }
+}
--- typescript/udemy-understanding-typescript/projects/updated-classes-interfaces-project/advanced.ts
+++ typescript/udemy-understanding-typescript/projects/updated-classes-interfaces-project/advanced.ts
@@ -1,10 +1,10 @@
 class AdvancedUser {
-  private _firstName: string = "";
+  protected _firstName: string = "";
   private _lastName: string = "";
 
   set firstName(name: string) {
     if (name.trim() === "") {
-      throw new Error("Invalid name");
+      throw new Error("Invalid first name");
     }
     this._firstName = name;
   }
@@ -31,5 +31,36 @@ console.log(AdvancedUser.eid); // Accessing static property
 
 const alv = new AdvancedUser();
 alv.firstName = "Alvin";
-alv.lastName = "Toh";
+alv.lastName = "Doe";
+// alv._firstName = "Alvin 2";
 console.log(alv.fullName);
+
+class Employee extends AdvancedUser {
+  constructor(public jobTitle: string) {
+    super();
+    // super.firstName = Alvin;
+  }
+
+  work() {
+    // ...
+    console.log(this._firstName);
+    // super._firstName
+  }
+}
+
+abstract class UIElement {
+  constructor(public identifier: string) {}
+
+  clone(targetLocation: string) {
+    // logic to duplicate the UI element
+  }
+}
+
+// let uiElement = new UIElement
+// Abstract classes should only be used as base classes to be extended from
+
+class SideDrawerElement extends UIElement {
+  constructor(public identifier: string, public position: "left" | "right") {
+    super(identifier);
+  }
+}
