227bde14bb6fd8d00bf85ef9a13afe27c67be3e1
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2025-05-29 23:37:03 +1000

    feat(section11): Finishing updated content for 2025 until 137. Building a Class Decorators That Edits a Class

---
+++ typescript/udemy-understanding-typescript/projects/section-11-ecmascript-decorators-project/decorators.ts
@@ -0,0 +1,25 @@
+function logger<T extends new (...args: any[]) => any>(
+  target: T,
+  ctx: ClassDecoratorContext
+) {
+  console.log("logger decorator");
+  console.log(target);
+  console.log(ctx);
+
+  return class extends target {
+    age = 35;
+  };
+}
+
+@logger
+class Person {
+  name = "Max";
+
+  greet() {
+    console.log("Hi, I am " + this.name);
+  }
+}
+
+const max = new Person();
+max.greet();
+console.log(max);
