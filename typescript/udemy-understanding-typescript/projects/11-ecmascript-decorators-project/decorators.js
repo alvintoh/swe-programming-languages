b970f66b6a818d7788d6583b23352cd572b2680d
ALVIN-DESKTOP/Alvin Toh <alvin4292@gmail.com>
2025-06-01 17:23:36 +1000

    feat(section12): Finishing updated content for 2025 until 145. Onwards to Experimental Decorators

--- typescript/udemy-understanding-typescript/projects/11-ecmascript-decorators-project/decorators.js
+++ typescript/udemy-understanding-typescript/projects/11-ecmascript-decorators-project/decorators.js
@@ -1,4 +1,11 @@
 "use strict";
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
 var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
     function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
     var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
@@ -26,13 +33,6 @@ var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, 
     if (target) Object.defineProperty(target, contextIn.name, descriptor);
     done = true;
 };
-var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
-    var useValue = arguments.length > 2;
-    for (var i = 0; i < initializers.length; i++) {
-        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
-    }
-    return useValue ? value : void 0;
-};
 var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
     if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
     return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
@@ -42,10 +42,32 @@ function logger(target, ctx) {
     console.log(target);
     console.log(ctx);
     return class extends target {
-        constructor() {
-            super(...arguments);
-            this.age = 35;
+        constructor(...args) {
+            super(...args);
+            console.log("class constructor");
+            console.log(this);
         }
+    };
+}
+function autobind(target, ctx) {
+    ctx.addInitializer(function () {
+        this[ctx.name] = this[ctx.name].bind(this);
+    });
+    return function () {
+        //Used to replace the original method
+        console.log("Executing original function");
+        target.apply(this);
+    };
+}
+function replacer(initValue) {
+    // Field replacerDecorator Decorator param target will always be undefined
+    return function replacerDecorator(target, ctx) {
+        console.log(target);
+        console.log(ctx);
+        return (initialValue) => {
+            console.log(initialValue);
+            return initValue;
+        };
     };
 }
 let Person = (() => {
@@ -53,17 +75,27 @@ let Person = (() => {
     let _classDescriptor;
     let _classExtraInitializers = [];
     let _classThis;
+    let _instanceExtraInitializers = [];
+    let _name_decorators;
+    let _name_initializers = [];
+    let _name_extraInitializers = [];
+    let _greet_decorators;
     var Person = _classThis = class {
-        constructor() {
-            this.name = "Max";
-        }
         greet() {
             console.log("Hi, I am " + this.name);
+        }
+        constructor() {
+            this.name = (__runInitializers(this, _instanceExtraInitializers), __runInitializers(this, _name_initializers, "Max"));
+            __runInitializers(this, _name_extraInitializers);
         }
     };
     __setFunctionName(_classThis, "Person");
     (() => {
         const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+        _name_decorators = [replacer("")];
+        _greet_decorators = [autobind];
+        __esDecorate(_classThis, null, _greet_decorators, { kind: "method", name: "greet", static: false, private: false, access: { has: obj => "greet" in obj, get: obj => obj.greet }, metadata: _metadata }, null, _instanceExtraInitializers);
+        __esDecorate(null, null, _name_decorators, { kind: "field", name: "name", static: false, private: false, access: { has: obj => "name" in obj, get: obj => obj.name, set: (obj, value) => { obj.name = value; } }, metadata: _metadata }, _name_initializers, _name_extraInitializers);
         __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
         Person = _classThis = _classDescriptor.value;
         if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
@@ -72,5 +104,5 @@ let Person = (() => {
     return Person = _classThis;
 })();
 const max = new Person();
-max.greet();
-console.log(max);
+const greet = max.greet;
+greet();
--- typescript/udemy-understanding-typescript/projects/11-ecmascript-decorators-project/decorators.ts
+++ typescript/udemy-understanding-typescript/projects/11-ecmascript-decorators-project/decorators.ts
@@ -7,19 +7,56 @@ function logger<T extends new (...args: any[]) => any>(
   console.log(ctx);
 
   return class extends target {
-    age = 35;
+    constructor(...args: any[]) {
+      super(...args);
+      console.log("class constructor");
+      console.log(this);
+    }
+  };
+}
+
+function autobind(
+  target: (...args: any[]) => any,
+  ctx: ClassMethodDecoratorContext
+) {
+  ctx.addInitializer(function (this: any) {
+    this[ctx.name] = this[ctx.name].bind(this);
+  });
+
+  return function (this: any) {
+    //Used to replace the original method
+    console.log("Executing original function");
+    target.apply(this);
+  };
+}
+
+function replacer<T>(initValue: T) {
+  // Field replacerDecorator Decorator param target will always be undefined
+  return function replacerDecorator(
+    target: undefined,
+    ctx: ClassFieldDecoratorContext
+  ) {
+    console.log(target);
+    console.log(ctx);
+
+    return (initialValue: any) => {
+      console.log(initialValue);
+      return initValue;
+    };
   };
 }
 
 @logger
 class Person {
+  @replacer("")
   name = "Max";
 
+  @autobind
   greet() {
     console.log("Hi, I am " + this.name);
   }
 }
 
 const max = new Person();
-max.greet();
-console.log(max);
+const greet = max.greet;
+greet();
