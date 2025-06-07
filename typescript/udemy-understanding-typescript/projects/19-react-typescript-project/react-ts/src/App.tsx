b6f369dc74dd3ca8981f9796a67fbb49d8df41e3
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-06-07 22:02:58 +1000

    feat(section19): finished until 244. Wrap Up

--- typescript/udemy-understanding-typescript/projects/19-react-typescript-project/react-ts/src/App.tsx
+++ typescript/udemy-understanding-typescript/projects/19-react-typescript-project/react-ts/src/App.tsx
@@ -1,12 +1,45 @@
 import goalsImg from "./assets/goals.jpg";
+import CourseGoals from "./components/CourseGoals.tsx";
 import Header from "./components/Header.tsx";
 
+import { useState } from "react";
+import NewGoal from "./components/NewGoal.tsx";
+
 function App() {
+  const [goals, setGoals] = useState([
+    {
+      id: 1,
+      title: "Learn TS",
+      description: "Learn TS from the ground up",
+    },
+    {
+      id: 2,
+      title: "Practice TS",
+      description: "Practice working with Typescript!",
+    },
+  ]);
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((g) => g.id !== id));
+  }
+
+  function handleAddGoal(text: string, summary: string) {
+    setGoals((prevGoals) =>
+      prevGoals.concat({
+        id: Math.random(),
+        title: text,
+        description: summary,
+      })
+    );
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>My Course Goals</h1>
       </Header>
+      <NewGoal onAdd={handleAddGoal} />
+      <CourseGoals goals={goals} onDelete={handleDeleteGoal} />
     </main>
   );
 }
---
+++ typescript/udemy-understanding-typescript/projects/19-react-typescript-project/react-ts/src/components/NewGoal.tsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+
+interface NewGoalProps {
+  onAdd: (text: string, summary: string) => void;
+}
+
+export default function NewGoal({ onAdd }: NewGoalProps) {
+  const goalRef = useRef<HTMLInputElement>(null);
+  const summaryRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const enteredGoal = goalRef.current!.value;
+    const enteredSummary = summaryRef.current!.value;
+
+    // validation...
+    onAdd(enteredGoal, enteredSummary);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="goal">Your goal</label>
+        <input id="goal" type="text" ref={goalRef} />
+      </p>
+      <p>
+        <label htmlFor="summary">Short summary</label>
+        <input id="summary" type="text" ref={summaryRef} />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
+  );
+}
